7584cacd4ebc943b667a98a9d22a5a3b09091cd3
Dimitri Kael Pires Vieira <dimitripiriswork@gmail.com>
2026-05-22 04:12:33 +0000

    refact: readequa o código para ler e tratar a base de dados que foi divida em 2 (linguagem R)

--- random_forest/random_forest.r
+++ random_forest/random_forest.r
@@ -1,40 +1,38 @@
-# Instale os pacotes se não os tiver no seu ambiente: install.packages(c("randomForest", "caret", "e1071"))
 library(randomForest)
 library(caret)
 
-# 1. Carregando os Dados
-df <- read.csv("ObesityDataSet_raw_and_data_sinthetic.csv")
+# 1. Carregando os Dados (Já separados na proporção 75/25)
+treino <- read.csv("treino.csv")
+teste  <- read.csv("teste.csv")
 
-# Tipagem estrita: Garantimos que o alvo (7 classes) e as colunas categóricas sejam Fatores
-df$Gender <- as.factor(df$Gender)
-df$family_history_with_overweight <- as.factor(df$family_history_with_overweight)
-df$FAVC <- as.factor(df$FAVC)
-df$CAEC <- as.factor(df$CAEC)
-df$SMOKE <- as.factor(df$SMOKE)
-df$SCC <- as.factor(df$SCC)
-df$CALC <- as.factor(df$CALC)
-df$MTRANS <- as.factor(df$MTRANS)
-df$NObeyesdad <- as.factor(df$NObeyesdad)
-# df$Sistemas <- as.factor(df$Sistemas) # Repita para suas outras colunas de texto
+# Lista das colunas que são texto/categorias e precisam virar Fatores
+colunas_categoricas <- c("Gender", "family_history_with_overweight", "FAVC", "CAEC", 
+                         "SMOKE", "SCC", "CALC", "MTRANS", "NObeyesdad")
 
-# Dividindo o banco (75/25)
-set.seed(42)
-indice_treino <- createDataPartition(df$NObeyesdad, p = 0.75, list = FALSE)
-treino <- df[indice_treino, ]
-teste  <- df[-indice_treino, ]
+# TRUQUE DE ALINHAMENTO NO R: Convertendo para fator e garantindo níveis iguais
+for(coluna in colunas_categoricas) {
+  # Transforma a coluna do treino em Fator
+  treino[[coluna]] <- as.factor(treino[[coluna]])
+  
+  # Transforma a coluna do teste forçando ela a ter as exatas categorias do treino
+  teste[[coluna]]  <- factor(teste[[coluna]], levels = levels(treino[[coluna]]))
+}
+
+# A etapa de createDataPartition foi removida pois as bases já estão divididas!
 
 # 2. O Treinamento da Floresta
-# A sintaxe "Classe ~ ." manda o algoritmo usar as 16 colunas restantes. ntree = 100 árvores.
-set.seed(42)
+# Travando a semente com o número 1, afetando a aleatoriedade da floresta
+set.seed(1) 
+
+# A sintaxe "NObeyesdad ~ ." manda usar todas as colunas para prever o alvo
 modelo_rf <- randomForest(NObeyesdad ~ ., data = treino, ntree = 100, importance = TRUE)
 
 # 3. Previsão e Avaliação
 previsoes <- predict(modelo_rf, teste)
 
-# A função do caret já gera a matriz e todo o relatório de recall/precision por classe de uma vez
+# Gerando a matriz de confusão e as métricas
 matriz_completa <- confusionMatrix(previsoes, teste$NObeyesdad)
 print(matriz_completa)
 
 # 4. Bônus: Gráfico de Importância
-# Isso vai abrir uma janela mostrando quais das suas 16 variáveis definem mais as regras de negócio
 varImpPlot(modelo_rf)
